f6f1dfb83ab759ab172de4b87e9e62ed1bb3a57e
xtbl <correodecristobal@gmail.com>
2015-08-11 16:44:57 -0600

    34 symbols

--- es6_training.js
+++ es6_training.js
@@ -1,5 +1,282 @@
+// 34: symbol
+// A symbol is a unique and immutable data type and may be used as an identifier for object properties
+// read more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol
 
-// 27: class - super inside a method
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('Symbol', function() {
+
+  it('`Symbol` lives in the global scope', function(){
+    const expected = Symbol;
+    assert.equal(Symbol, expected);
+  });
+
+  it('every `Symbol()` is unique', function(){
+    const sym1 = Symbol();
+    const sym2 = Symbol();
+    assert.notEqual(sym1, sym2);
+  });
+
+  it('every `Symbol()` is unique, also with the same parameter', function(){
+    var sym1 = Symbol('foo');
+    var sym2 = Symbol('foo');
+    assert.notEqual(sym1, sym2);
+  });
+
+  it('`typeof Symbol()` returns "symbol"', function(){
+    const theType = typeof Symbol();
+    assert.equal(theType, 'symbol');
+  });
+
+  it('`new Symbol()` throws an exception, to prevent creation of Symbol wrapper objects', function(){
+    function fn() {
+      new Symbol();
+    }
+    assert.throws(fn);
+  });
+
+});
+
+
+
+33: array - `Array.prototype.findIndex` 
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.prototype.findIndex` makes finding items in arrays easier', () => {
+
+  it('takes a compare function, returns the index where it returned true', function() {
+    const foundAt = [false, true].findIndex(item => item === true);
+    
+    assert.equal(foundAt, 1);
+  });
+
+  it('returns the first position it was found at', function() {
+    const foundAt = [0, 1, 1, 1].findIndex(item => item === 1);
+    
+    assert.equal(foundAt, 1);
+  });
+
+  it('returns `-1` when nothing was found', function() {
+    const foundAt = [1, 2, 3].findIndex(item => item > 3);
+    
+    assert.equal(foundAt, -1);
+  });
+
+  it('the findIndex callback gets the item, index and array as arguments', function() {
+    const three = 3;
+    const containsThree = arr => arr.indexOf(three) > -1;
+    function theSecondThree(item, index, arr) {
+      const preceedingItems = arr.slice(0, index);
+      return containsThree(preceedingItems);
+    }
+    const foundAt = [1, 1, 2, 3, 3, 3].findIndex(theSecondThree);
+    
+    assert.equal(foundAt, 4);
+  });
+
+  it('combined with destructuring complex compares become short', function() {
+    const bob = {name: 'Bob'};
+    const alice = {name: 'Alice'};
+    const foundAt = [bob, alice].findIndex(({name:{length}}) => length > 3);
+    
+    assert.equal(foundAt, 1);
+  });
+
+});
+
+
+// 32: array - `Array.prototype.find` 
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.prototype.find` makes finding items in arrays easier', () => {
+
+  it('takes a compare function', function() {
+    const found = [false, true].find(item => item === true);
+    
+    assert.equal(found, true);
+  });
+
+  it('returns the first value found', function() {
+    const found = [0, 2].find(item => item > 1);
+    
+    assert.equal(found, 2);
+  });
+
+  it('returns `undefined` when nothing was found', function() {
+    const found = [1, 2, 3].find(item => item === 4);
+    
+    assert.equal(found, void 0);
+  });
+
+  it('combined with destructuring complex compares become short', function() {
+    const bob = {name: 'Bob'};
+    const alice = {name: 'Alice'};
+    const found = [alice, bob].find(({name:{length}}) => length);
+    assert.equal(found, alice);
+  });
+
+});
+
+// 31: array - `Array.prototype.fill` method
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.prototype.fill` can fill up an array with one value', () => {
+
+  it('`fill(0)` will populate `0` into each array element', function() {
+    const arr = new Array(3).fill(0);
+    
+    assert.deepEqual(arr, [0, 0, 0]);
+  });
+
+  it('fill only changes content, adds no new elements', function() {
+    const arr = [].fill(0);
+    
+    assert.deepEqual(arr, []);
+  });
+
+  it('second parameter to `fill()` is the position where to start filling', function() {
+    const fillPosition = 2;
+    const arr = [1,2,3].fill(42, fillPosition);
+    
+    assert.deepEqual(arr, [1, 2, 42]);
+  });
+
+  it('third parameter is the position where filling stops', function() {
+    const fillStartAt = 1;
+    const fillEndAt = 2;
+    const arr = [1,2,3].fill(42, fillStartAt, fillEndAt);
+    
+    assert.deepEqual(arr, [1, 42, 3]);
+  });
+
+});
+
+
+// 30: array - `Array.of` static method
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.of` creates an array with the given arguments as elements', () => {
+  
+  it('dont mix it up with `Array(10)`, where the argument is the array length', () => {
+    const arr = Array.of(10);
+    
+    assert.deepEqual(arr, [10]);
+  });
+  
+  it('puts all arguments into array elements', () => {
+    const arr = Array.of(1,2);
+    
+    assert.deepEqual(arr, [1, 2]);
+  });
+  
+  it('takes any kind and number of arguments', () => {
+    const starter = [1, 2];
+    const end = [3, '4'];
+    const arr = Array.of(starter, ...end);
+    
+    assert.deepEqual(arr, [[1, 2], 3, '4']);
+  });
+  
+});
+
+// 29: array - `Array.from` static method
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.from` converts an array-like object or list into an Array', () => {
+
+  const arrayLike = {0: 'one', 1: 'two', length: 2};
+  
+  it('call `Array.from` with an array-like object', function() {
+    const arr = Array.from(arrayLike);
+    
+    assert.deepEqual(arr, ['one', 'two']);
+  });
+  
+  it('a DOM node`s classList object can be converted', function() {
+    document.body.classList.add('some');
+    document.body.classList.add('other');
+    const classList = Array.from(document.body.classList);
+
+    assert.equal(''+classList, ''+['some', 'other']);
+  });
+  
+  it('convert a NodeList to an Array and `filter()` works on it', function() {
+    const nodeList = Array.from(document.querySelectorAll('body'));
+    const bodies = nodeList.filter((node) => node === document.body);
+    
+    assert.deepEqual(bodies, [document.body]);
+  });
+  
+  describe('custom conversion using a map function as second param', () => {
+    it('we can modify the value before putting it in the array', function() {
+      const arr = Array.from(arrayLike, (value) => value.toUpperCase());
+      assert.deepEqual(arr, ['ONE', 'TWO']);
+    });
+    
+    it('and we also get the object`s key as second parameter', function() {
+      const arr = Array.from(arrayLike, (value, key) => `${key}=${value}`);
+      assert.deepEqual(arr, ['0=one', '1=two']);
+    });
+  });
+  
+});
+
+// 28: class - super in constructor
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('class', () => {
+
+  it('if you `extend` a class, use `super()` to call the parent constructor', () => {
+    class A {constructor() { this.levels = 1; }}
+    class B extends A {
+      constructor() {
+        super();
+        this.levels++;
+      }
+    }
+    
+    assert.equal(new B().levels, 2);
+  });
+
+  it('`super()` may also take params', () => {
+    class A {constructor(startValue=1, addTo=1) { this.counter = startValue + addTo; }}
+    class B extends A {
+      constructor(...args) { 
+        super(...args);
+        this.counter++; 
+      }
+    }
+    
+    assert.equal(new B(42, 2).counter, 45);
+  });
+  
+  it('it is important where you place your `super()` call!', () => {
+    class A {inc() { this.countUp = 1; }}
+    class B extends A {
+      inc() {
+        this.countUp = 2;  
+        super.inc();
+        return this.countUp;
+      }
+    }
+    
+    assert.equal(new B().inc(), 1);
+  });
+
+  it('use `super.constructor` to find out if there is a parent constructor', () => {
+    class A extends null {
+      constructor() {
+        super();
+        this.isTop = !super.constructor;
+      }
+    }
+
+    assert.equal(new A().isTop, false);
+  });
+  
+});
+
+27: class - super inside a method
 // To do: make all tests pass, leave the assert lines unchanged!
 
 describe('inside a class use `super` to access parent methods', () => {
